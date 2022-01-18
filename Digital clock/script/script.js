9abc8de24919a2815c57b7dce71f414d90df79d4
Gaurav Jha <51743345+gauravjha9@users.noreply.github.com>
2022-01-18 11:07:01 +0530

    Add files via upload

---
+++ Digital clock/script/script.js
@@ -0,0 +1,31 @@
+const showTime = () => {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let time = `${h}:${m}:${s} `;
+
+  document.getElementById("clockDisplay").innerText = time;
+  document.getElementById("clockDisplay").textContent = time;
+  document.getElementById("timeFormat").innerText = session;
+
+  setTimeout(showTime, 1000);
+};
+
+showTime();
